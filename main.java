97190682b3cff47be59a9d26f3d5ae31811f019c
Ziad sakr <zee@147dda6addc1.student.trincoll.edu>
2020-11-01 12:43:20 -0500

    adding turnaround time fct

--- main.java
+++ main.java
@@ -1,5 +1,46 @@
+/*
+* Operating System Simulation
+* Systems Software Class for CPSC 315
+* @professor Dr.S
+* @authors Ziad Sakr, Aadiv Sheth
+* @Date: Sunday November 1st 2020
+*/
 public class main {
 
+	/*
+	* Function to Calcuate turnaround time
+	* @params process
+	* @params size
+	* @params burst: an array of burst time
+	* @params waiting: an array of the waiting time
+	* @params quantum
+	*/
+	public void waitingTime(int process[], int size, int burst[], int wait[], int quantum){
+		//Calcualte turnaround time
+
+	}
+
+	/*
+	* Function to Calcuate turnaround time
+	* @params process
+	* @params size
+	* @params burst: an array of burst time
+	* @params waiting: an array of the waiting time
+	* @params total an array to store the sum
+	*/
+	public void findTurnAroundTime(int process[], int size, int burst[], int wait[], int total[]){
+		//Calcualte turnaround time
+		for(int i = 0; i < size; i++){
+			total[i] = burst[i] + wait[i];
+		}
+	}
+
+
+
+
+	/*
+	* Main Function
+	*/
 
 	public static void main(String[] args) {
 
