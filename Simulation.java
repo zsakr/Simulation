8d54d203db8560e95a528cd25669f5b57c73987b
AadivSheth <73758491+AadivSheth@users.noreply.github.com>
2020-11-08 12:06:21 -0500

    Add files via upload

--- Simulation.java
+++ Simulation.java
@@ -3,71 +3,72 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Simulation {
-    static ArrayList<Process> ProcessesList = new ArrayList<Process>();//In list
-        
-    static ArrayList<Process> H1 = new ArrayList<Process>();//High priority list
-    static ArrayList<Process> L2 = new ArrayList<Process>();//first low priority list
-    static ArrayList<Process> L3 = new ArrayList<Process>();//second low priority list
-    static ArrayList<Process> Blocked = new ArrayList<Process>();//blocked queue
-    static ArrayList<Character> ganntChart = new ArrayList<Character>(); //gannt chart list
 
+    // Constant Variables
+    static final int MAX_BURST = 20;            // Maximum CPU bursts and blocks a process can have
+    static final int MAX_PREEMPTED = 3;         // Maximum number of times a process can be preempted before it is demoted
+    static final int GC_LINE_SIZE = 40;         // Maximum CPU bursts and blocks a process can have
+    static final int GC_FORMAT = 5;             // Maximum CPU bursts and blocks a process can have
+    static final char CPU_Idle = '*';           // Symbol for Gannt Chart when the CPU is Idle
+    static final String HIGH = "HP";            // Key for Process with High Priority/on H1 Queue
+    static final String LOW = "LP";             // Key for Process with Low Priority
+    static final String LOW2 = "L2";            // Key for Process on L2 Queue
+    static final String LOW3 = "L3";            // Key for Process on L3 Queue
+    static final String READY = "READY";        // Key for Process in Ready State (on one of the Ready Queues)
+    static final String RUNNING = "RUNNING";    // Key for Process in Running State (on the CPU)
+    static final String BLOCKED = "BLOCKED";    // Key for Process in Blocked State (on the Blocked Queue)
+    static final String DONE = "DONE";          // Key for Process that has left the system
+
+
+    // ArrayLists to hold processes and information
+    static ArrayList<Process> ProcessesList = new ArrayList<Process>();     // Global list of all Processes
+    static ArrayList<Process> H1 = new ArrayList<Process>();                // High Priority Ready Queue
+    static ArrayList<Process> L2 = new ArrayList<Process>();                // Low Priority 2 Ready Queue
+    static ArrayList<Process> L3 = new ArrayList<Process>();                // Low Priority 3 Ready Queue
+    static ArrayList<Process> Blocked = new ArrayList<Process>();           // Blocked Queue
+    static ArrayList<Character> ganntChart = new ArrayList<Character>();    // Gannt Chart
+
+    // Object to hold Process currently on the CPU
     static Process cur_running;
 
-    static int MAX_BURST = 20;
-    static int MAX_PREEMPTED = 3;
-    static String ready = "READY";
-    static String running = "RUNNING";
-    static String blocked = "BLOCKED";
-    static String done = "DONE";
-
-    static String high = "HP";
-    static String low = "LP";
-    static String low2 = "L2";
-    static String low3 = "L3";
-
-    static int inL3;
-    static int L2quant = 0;
-    static int L3quant = 0;
-    static char CPU_Idle = '*'; //* indicates idle CPU
-    static int clockTick = -1; //set to -1 initially
+    // Variables to hold Quantum Sizes
+    static int L2quant = 0;     // Quantime Size for L2 Queue
+    static int L3quant = 0;     // Quantime Size for L3 Queue
 
     public static void main(String args[])throws Exception {
-        if (args.length == 0) {//if no file is passed on the command line
+        // Check if input file was not passed as an argument in command line
+        if (args.length == 0) {
             System.out.println("No command line arguments were passed.");
             System.exit(0);
         }
 
         else {
-            File file = new File(args[0]);
-            readFile(file);
+            // Read input text
+            File file = new File(args[0]);          // Create a file to hold input text
+            readFile(file);                         // Function call to read the file holding input text
 
-            //run simulation
-            while (true) {
-                clockTick++;
-                CheckNewJobs(clockTick);
-                BlockedJobReturnCheck(clockTick);
-                CPUJobDoneCheck(clockTick);
-                QuantumCheck(clockTick);
-                PickProcess(clockTick);
-                UpdateWaitingTime();
+            // Run Simulation
+            int clockTick = -1;                     // Set clock tick to -1 intially
+            while (true) {                          // while loop to run simulation
+                clockTick++;                        // Increment clock tick by 1
+                CheckNewJobs(clockTick);            // Function call to check if new jobs are entering the system
+                BlockedJobReturnCheck(clockTick);   // Function call to check if block jobs are returning to the Ready Queues
+                CPUJobDoneCheck(clockTick);         // Function call to check if job currently running on the CPU has finished its CPU Burst
+                QuantumCheck(clockTick);            // Function call to check if job currently running on the CPU has finished it Quantum
+                PickProcess(clockTick);             // Function call to pick a process to run on the CPU if the CPU is Idle
+                UpdateWaitingTime();                // Function call to update the waiting time for each process
 
-                if (CheckExit())
+                if (CheckExit())                    // Function call to check if all the processes have left the system
                     break;
-                UpdateGanntChart();
+                UpdateGanntChart();                 // Function call to update the Gannt Chart at the end of each clock tick
             }
-            Output();
+            Print();                                // Function call to print Gannt Chart and other information
         }
-    }
-
-    public static void print(ArrayList<Process> list) {
-        for (int i=0; i<list.size(); i++) 
-            System.out.print(list.get(i).name + " ");
-        System.out.println();
     }
 
     public static void readFile(File file) {
         try {
-            Scanner scanner = new Scanner(file);
+            Scanner scanner = new Scanner(file);    // Scanner object 
 
             L2quant = scanner.nextInt();
             scanner.nextLine();
@@ -111,13 +112,13 @@ public class Simulation {
         for (int i=0; i<ProcessesList.size(); i++) {
             Process p = ProcessesList.get(i);
             if (p.in_time == clockTick) {
-                if (p.priority.equals(high)) {
-                    p.state = ready;
+                if (p.priority.equals(HIGH)) {
+                    p.state = READY;
                     H1.add(p);
                 }
                 else {
-                    p.state = ready;
-                    p.priority = low2;
+                    p.state = READY;
+                    p.priority = LOW2;
                     L2.add(p);
                 }
             }
@@ -127,18 +128,18 @@ public class Simulation {
     public static void BlockedJobReturnCheck(int clockTick) {
         for (int i=0; i<Blocked.size(); i++) {
             Process p = Blocked.get(i);
-            if (p.state.equals(blocked)) {
+            if (p.state.equals(BLOCKED)) {
                 if (p.blocked_till == clockTick) {
-                    if (p.priority.equals(high)) {
-                        p.state = ready;
+                    if (p.priority.equals(HIGH)) {
+                        p.state = READY;
                         p.burst_index++;
                         H1.add(p);
                         Blocked.remove(i);
                     }
                     else {
-                        p.state = ready;
+                        p.state = READY;
                         p.burst_index++;
-                        p.priority = low2;
+                        p.priority = LOW2;
                         L2.add(p);
                         Blocked.remove(i);
                     }
@@ -153,11 +154,11 @@ public class Simulation {
                 cur_running.burst_index++;
                 if (cur_running.burst_info[cur_running.burst_index] == -1) {
                     cur_running.out_time = clockTick;
-                    cur_running.state = done;
+                    cur_running.state = DONE;
                     cur_running = null;
                 }
                 else {
-                    cur_running.state = blocked;
+                    cur_running.state = BLOCKED;
                     cur_running.blocked_till = clockTick + cur_running.burst_info[cur_running.burst_index];
                     cur_running.num_preempted = 0;
                     cur_running.run_time = 0;
@@ -172,13 +173,13 @@ public class Simulation {
 
     public static void QuantumCheck(int clockTick) {
         if (cur_running != null) {
-            if (cur_running.priority.equals(low2)) {
+            if (cur_running.priority.equals(LOW2)) {
                 if (cur_running.run_time == L2quant) {
                     cur_running.num_preempted++;
                     cur_running.burst_info[cur_running.burst_index] -= L2quant;
-                    cur_running.state = ready;
+                    cur_running.state = READY;
                     if (cur_running.num_preempted == MAX_PREEMPTED) {
-                        cur_running.priority = low3;
+                        cur_running.priority = LOW3;
                         L3.add(cur_running);
                         cur_running = null;
                     }
@@ -188,10 +189,10 @@ public class Simulation {
                     }
                 }
             }
-            else if (cur_running.priority.equals(low3)) {
+            else if (cur_running.priority.equals(LOW3)) {
                 if (cur_running.run_time == L3quant) {
                     cur_running.burst_info[cur_running.burst_index] -= L3quant;
-                    cur_running.state = ready;
+                    cur_running.state = READY;
                     L3.add(cur_running);
                     cur_running = null;
                 }
@@ -203,7 +204,7 @@ public class Simulation {
         if (cur_running == null) {
             if (!H1.isEmpty()) {
                 cur_running = H1.remove(0);
-                cur_running.state = running;
+                cur_running.state = RUNNING;
                 cur_running.run_till = clockTick + cur_running.burst_info[cur_running.burst_index];
                 cur_running.run_time = 0;
                 //ganntChart.add(cur_running.name);
@@ -211,14 +212,14 @@ public class Simulation {
 
             else if (!L2.isEmpty()) {
                 cur_running = L2.remove(0);
-                cur_running.state = running;
+                cur_running.state = RUNNING;
                 cur_running.run_till = clockTick + cur_running.burst_info[cur_running.burst_index];
                 cur_running.run_time = 0;
                 //ganntChart.add(cur_running.name);
             }
             else if (!L3.isEmpty()) {
                 cur_running = L3.remove(0);
-                cur_running.state = running;
+                cur_running.state = RUNNING;
                 cur_running.run_till = clockTick + cur_running.burst_info[cur_running.burst_index];
                 cur_running.run_time = 0;
               //  ganntChart.add(cur_running.name);
@@ -231,7 +232,7 @@ public class Simulation {
     public static void UpdateWaitingTime() {
         for (int i=0; i<ProcessesList.size(); i++) {
             Process p = ProcessesList.get(i);
-            if (p.state.equals(ready))
+            if (p.state.equals(READY))
                 p.wait_time++;
         }
     }
@@ -246,12 +247,12 @@ public class Simulation {
     public static boolean CheckExit() {
         boolean b = true;
         for (int i=0; i<ProcessesList.size(); i++)
-            if (!ProcessesList.get(i).state.equals(done))
+            if (!ProcessesList.get(i).state.equals(DONE))
                 b = false;
         return b;
     }
 
-    public static void Output() {
+    public static void Print() {
         String gaant = "";
         double idle_time = 0;
         double total_time = ganntChart.size();
